dbb8add040fad6cfa11c1ad93b1860438281ea17
Bayan <Bayan@DESKTOP-COKKPVL>
2024-07-11 11:34:25 +0300

    Indexer property

--- OOP_2/Program.cs
+++ OOP_2/Program.cs
@@ -1,25 +1,69 @@
-﻿namespace OOP_2
+﻿using System.Security.Cryptography;
+
+namespace OOP_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
 
+            #region MyRegion
             Employee emp = new Employee();
 
             emp.SetName("Bayan");
 
-            Console.WriteLine(emp.GetName());
+            //Console.WriteLine(emp.GetName());
 
 
 
             //Full time proprrty
 
             emp.Salary = 2000;
-            Console.WriteLine(emp.Salary);
+            //Console.WriteLine(emp.Salary);
 
             decimal ded = emp.dedaction;
-            Console.WriteLine(ded);
+           // Console.WriteLine(ded);
+            #endregion
+
+            #region PhoneBook With Struct
+
+            PhoneBook c1 = new PhoneBook(3);
+            c1.AddPerson(0, "Bayan", 123);
+            c1.AddPerson(1, "OMar", 456);
+            c1.AddPerson(2, "rayan", 789);
+
+           // Console.WriteLine(c1.GetNumber("Bayan"));
+            #endregion
+
+            #region Indexer property
+            c1["Bayan"] = 101001;
+            Console.WriteLine(c1["Bayan"]);
+            #endregion
+
+            for (int i = 0; i < c1.Size; i++)
+            {
+
+                Console.WriteLine(c1[i]);
+
+
+
+
+            }
+
+
+
+
         }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
